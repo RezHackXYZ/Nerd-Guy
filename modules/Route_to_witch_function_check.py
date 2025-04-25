4d5beab9a09b14e5dfbcc4d754d8ab31776d8c31
RezHackXYZ <rhythmupa@gmail.com>
2025-04-25 13:17:17 +0530

    fsdsf

--- modules/Route_to_witch_function_check.py
+++ modules/Route_to_witch_function_check.py
@@ -1,5 +1,5 @@
 from modules.Real_function_Exicution import send_message, aiprompt
-
+from modules.none import none
 
 def FindWhatType(text, channel_id, ts, thread_ts, user):
     if user == "USLACKBOT":
@@ -7,14 +7,16 @@ def FindWhatType(text, channel_id, ts, thread_ts, user):
 
     elif text.replace("<@U08P7D71MRU> ", "") == "ping":
         send_message(channel_id, "🏓 PONG! 🏓", ts)
-
     else:
         Type = FindType(text)
         send_message(
             channel_id,
-            f"Type: `{Type}`",
+            f"Type: {Type}",
             ts,
         )
+
+        if "none" in Type:
+            none(text, channel_id, ts)
 
 
 def FindType(text):
@@ -27,8 +29,9 @@ Choose whether this query needs:
 - context: The query is incomplete and needs more context, or the user is asking for a summary, TL;DR, or something similar.
 - search: The user is asking for something that needs to be searched within the Slack workspace, like “What are the new announcements?” Queries that cannot be answered using Slack (e.g., “How many moons does Jupiter have?”) should not be classified as search, because they require searching the internet, not the Slack workspace.
 - none: The query is complete and does not need any additional context or search.
-Ignore <@U08P7D71MRU> — it's your Slack bot ID being pinged!
-Respond with ONLY the type of the query, nothing else, no newline, no extra text, no explanation.
+Ignore <@...> — it's your Slack bot ID being pinged!
+Respond with ONLY the type of the query, nothing else, no newline, no extra text, no explanation, AND NO EXTRA SPACES.
+If you are confuzed or the user query dosint make any sense, classify it as "none".
 
 The query is:
 {query}
